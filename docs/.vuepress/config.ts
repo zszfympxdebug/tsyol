38d9bc06e39ddd8f88481a70c2de0712440c8280
zszfympxdebug <denys2010@163.com>
2023-10-05 15:00:31 +0800

    Vuepress Init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,24 @@
+import {defineUserConfig} from 'vuepress';
+import {katex} from '@mdit/plugin-katex';
+import {mdEnhancePlugin} from 'vuepress-plugin-md-enhance'
+
+export default defineUserConfig({
+    lang: 'zh_CN',
+    title: 'Demo',
+    description: 'Vuepress Demo!',
+    extendsMarkdown: (md) => {
+        md.use(katex),
+        md.linkify.set({fuzzyEmail: false})
+    },
+    head: [
+        ["link", {rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css"},],
+        ["link", {rel: "text/javascript", href: "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.js",},],
+        ["link", {rel: "icon", href: "./icon.png",},],
+    ],
+    plugins: [
+        mdEnhancePlugin({
+            katex: true,
+            mathjax: false,
+        }),
+    ],
+})  
